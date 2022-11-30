a9799c13c2b99ffdf34b9622e209d6af7e815d88
Kazunori Osaki <claudioropez.k@gmail.com>
2022-11-30 17:33:03 +0900

    Add eslint setting

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: ["next", "turbo", "prettier", "next/babel"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "prettier/prettier": "error",
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-unused-vars": [
+      2,
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
   },
 };
