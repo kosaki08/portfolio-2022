ac4be6e2659ef4b34f295ea4dd5ea9cebfc5d0e5
Kazunori Osaki <claudioropez.k@gmail.com>
2022-12-01 10:46:06 +0900

    Update prettier setting

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   // Lint then format TypeScript and JavaScript files
   'app/**/*.(ts|tsx|js)': (filenames) => [
-    `pnpm  lint --fix ${filenames.join(" ")}`, // prettier-ignore
-    `pnpm  format --write ${filenames.join(' ')}`,
+    `pnpm lint --fix ${filenames.join(" ")}`, // prettier-ignore
+    `pnpm format --write ${filenames.join(' ')}`,
   ],
 
   // Format MarkDown and JSON
   "**/*.(md|json)": (filenames) => 
-    `pnpm  format --write ${filenames.join(" ")}`, // prettier-ignore
+    `pnpm format --write ${filenames.join(" ")}`, // prettier-ignore
 };
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,18 +1,18 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["next", "turbo", "prettier", "next/babel"],
+  extends: ['next', 'turbo', 'prettier', 'next/babel'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-    "prettier/prettier": "error",
-    "@typescript-eslint/consistent-type-imports": "off",
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "@typescript-eslint/no-unused-vars": [
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/consistent-type-imports': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': [
       2,
       {
-        argsIgnorePattern: "^_",
-        varsIgnorePattern: "^_",
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
       },
     ],
   },
